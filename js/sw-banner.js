1f2fa06ad4fa3de26cc1c7fd899191ed48852fb6
Kwononju <onjuu1016@gmail.com>
2025-05-29 12:35:50 +0900

    design:main-left PC화면 영역 작성 완료

---
+++ js/sw-banner.js
@@ -0,0 +1,10 @@
+ const swBanner = new Swiper(".sw-Swiper", {
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
